e844124a8c9bda1adb0ac9ebfc9b3821c59b77e6
Bikaros2540 <mbkar2540@gmail.com>
2026-04-08 17:50:29 +0200

    Add solution for selu-activation

---
+++ selu-activation/selu-activation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def selu(x, lam=1.0507009873554804934193349852946, alpha=1.6732632423543772848170429916717):
+    """
+    Apply SELU activation element-wise.
+    Returns a list of floats rounded to 4 decimal places.
+    """
+    z =[ lam*v if v > 0 else lam*alpha*(math.exp(v) - 1) for v in x]
+    return z
+    pass
